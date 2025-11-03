500bd44cf1b27fa46c45dea8209f7ec4b45b901a
DIEGO DE PADUA BEZERRA DE LEMOS <sn1094808@cfp127aluno.local>
2025-11-03 11:27:57 -0300

    Iniciando o desenvolvimento da tabuada

--- src/main/java/br/senai/sp/jandira/tabuada/HelloApplication.java
+++ src/main/java/br/senai/sp/jandira/tabuada/HelloApplication.java
@@ -3,17 +3,59 @@ package br.senai.sp.jandira.tabuada;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        //Definir tamanho da tela
+        stage.setWidth(500);
+        stage.setHeight(500);
+
+        //Componente principal da tela
+        VBox root = new VBox();
+        Scene scene = new Scene(root);
+
+
         stage.setScene(scene);
+        stage.setTitle("Tabuada");
         stage.show();
+
+        //Cabeçalho da tela
+        VBox header = new VBox();
+        header.setStyle("-fx-padding: 10;-fx-background-color: rgba(106,0,0,0.96)");
+
+        //Adicionar um label ao header
+        Label labelTitulo = new Label("Tabuada");
+        labelTitulo.setStyle("-fx-text-fill: white;-fx-font-size: 30;-fx-font-weight: bold;");
+        Label labelSubtitulo = new Label("Construa tabuadas sem limites!");
+        labelSubtitulo.setStyle("-fx-text-fill: #ffe0e6;-fx-font-size: 15");
+
+        header.getChildren().add(labelTitulo);
+        header.getChildren().add(labelSubtitulo);
+
+        //Colocar o header no root
+        root.getChildren().addAll(header);
+
+        //Criar o multiplicando
+        HBox multiplicandoBox = new HBox();
+         multiplicandoBox.setStyle("-fx-padding: 10;");
+         Label labelMultiplicando = new Label("Multiplicando");
+        TextField textFieldMultiplicando = new TextField();
+
+        multiplicandoBox.getChildren().add(textFieldMultiplicando);
+        multiplicandoBox.getChildren().add(multiplicandoBox);
+
+
+        //Colocamos o multiplicandoBox no root
+        root.getChildren().add(multiplicandoBox);
+
+
     }
 }
